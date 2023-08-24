88954d14fb1a0d540f21ff25f27f080a893ec8d2
Magno Valdetaro <mvaldetaro@gmail.com>
2023-08-24 13:48:53 -0300

    Utiliza InfoCard

--- src/components/TotalCard/TotalCard.jsx
+++ src/components/TotalCard/TotalCard.jsx
@@ -1,28 +1,32 @@
-import React, { memo } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { formats } from "investira.sdk";
-import { Stack, Card, CardContent, Typography } from "@mui/material";
+import { InfoCard } from "../";
 import utils from "../../utils";
 
-const TotalCard = memo((props) => {
+const TotalCard = (props) => {
   const calculatedValue = utils.calc.somaAttr(props.data, props.options);
   const formatedValue = formats.formatNumber(calculatedValue, 2, true, true);
 
   return (
-    <Stack sx={{ ...props.sx }} flexGrow={1}>
-      <Card variant="outlined" sx={{ height: "100%" }}>
-        <CardContent>
-          <Typography variant="caption" gutterBottom>
-            {props.label || "label"}
-          </Typography>
-          <Typography variant="h5" component="div">
-            {props.value || formatedValue || "0"}
-          </Typography>
-        </CardContent>
-      </Card>
-    </Stack>
+    <InfoCard
+      label={props.label}
+      value={props.value || formatedValue || "Sem dados"}
+    />
   );
-});
+};
 
 TotalCard.displayName = "TotalCard";
+
+TotalCard.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  data: PropTypes.array,
+  options: PropTypes.shape({
+    soma: PropTypes.string,
+    filter: PropTypes.string,
+    filter_value: PropTypes.string,
+  }),
+};
 
 export default TotalCard;
