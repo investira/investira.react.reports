74e167656038a53d88aaee4932df7bc5cf2bffe5
Magno Valdetaro <mvaldetaro@gmail.com>
2023-08-24 13:48:30 -0300

    Create InfoCard

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ export * from "./wrappers/";
 export { default as Ativos } from "./Ativos";
 export { default as Breaks } from "./Breaks";
 export { default as Icon } from "./Icon";
+export { default as InfoCard } from "./InfoCard";
 export { default as Document } from "./Document";
 export { default as TableContent } from "./TableContent";
 export { default as TableHeader } from "./TableHeader";
