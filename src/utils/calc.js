fdbac96939c2ccfc751291574cacc62c0ff1397d
Magno Valdetaro <mvaldetaro@gmail.com>
2023-08-29 18:16:52 -0300

    Implementa mediaPonderada()

--- src/utils/calc.js
+++ src/utils/calc.js
@@ -1,34 +1,52 @@
+import { validators } from "investira.sdk";
+
 function somaAttr(pData = [], pOptions = {}) {
   if (!pOptions.hasOwnProperty("soma")) {
     console.error("Atributo soma deve ser informado em options");
     return 0;
   }
 
-  if (!pOptions.hasOwnProperty("filter")) {
-    console.error("Atributo filter deve ser informado em options");
-    return 0;
+  let xFilteredData = pData;
+
+  if (
+    pOptions.hasOwnProperty("filter") &&
+    pOptions.hasOwnProperty("filter_value")
+  ) {
+    xFilteredData = pData.filter((xItem) => {
+      return xItem[pOptions.filter] === pOptions.filter_value;
+    });
   }
-
-  if (!pOptions.hasOwnProperty("filter_value")) {
-    console.error("Atributo filter_value deve ser informado em options");
-    return 0;
-  }
-
-  const { soma, filter, filter_value } = pOptions;
-
-  const xFilteredData = pData.filter((xItem) => {
-    return xItem[filter] === filter_value;
-  });
 
   const xSoma = xFilteredData.reduce((acc, curr) => {
-    return acc + curr[soma];
+    return acc + curr[pOptions.soma];
   }, 0);
 
   return xSoma;
 }
 
+function mediaPonderada(pData = [], pCols = []) {
+  if (validators.isEmpty(pData) || validators.isEmpty(pCols)) {
+    return 0;
+  }
+
+  // Soma coluna
+  const xSoma = somaAttr(pData, { soma: pCols[0] });
+
+  // Multiplica colunas e soma linha por linha
+  const xMult = pData.reduce((acc, curr) => {
+    const xResult = acc + curr[pCols[0]] * curr[pCols[1]];
+    return xResult;
+  }, 0);
+
+  // Calcula media
+  const xResult = xMult / xSoma;
+
+  return xResult;
+}
+
 const calc = {
   somaAttr,
+  mediaPonderada,
 };
 
 export default calc;
